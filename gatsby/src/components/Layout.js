6b091a92bb3c083ad2cb281e1368fddc8e9b6145
Vinh Do <vinhdo07@gmail.com>
2020-10-11 21:32:38 -0700

    styling

--- gatsby/src/components/Layout.js
+++ gatsby/src/components/Layout.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import Footer from './Footer';
 import Nav from './Nav';
+import 'normalize.css';
+import GlobalStyles from '../styles/GlobalStyles';
+import Typography from '../styles/Typography';
 
 export default function Layout({ children }) {
   return (
     <div>
+      <GlobalStyles />
+      <Typography />
       <Nav />
       <p>I'm the page's contents.</p>
       {children}
